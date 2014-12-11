68626742f673a942b019f198113ea4ddabdb05d8
Pranav Salunke <dguitarbite@gmail.com>
2014-12-11 10:12:18 +0100

    Adds support to launch VM

--- stacktrain/launch_vm.py
+++ stacktrain/launch_vm.py
@@ -2,5 +2,57 @@ import libvirt
 import sys
 from xml.etree import ElementTree as ET
 
-# Do more stuff here!
+uri = 'qemu:///system'
+conn = libvirt.open(uri)
+<<<<<<< HEAD
 
+
+# Get Libvirt version
+# getversion
+# getLibVersion
+
+# Get CPU information
+# getCPUMap
+# getMaxVcpus
+
+# Get free memory
+# getFreeMemory
+# getMemoryParameters
+
+#
+
+'''
+
+import libvirt
+conn=libvirt.open("qemu:///system")
+
+for id in conn.listDomainsID():
+    dom = conn.lookupByID(id)
+    infos = dom.info()
+    print 'ID = %d' % id
+    print 'Name =  %s' % dom.name()
+    print 'State = %d' % infos[0]
+    print 'Max Memory = %d' % infos[1]
+    print 'Number of virt CPUs = %d' % infos[3]
+    print 'CPU Time (in ns) = %d' % infos[2]
+    print ' '
+
+'''
+
+=======
+>>>>>>> 3b50bc5ab0b86c254422c649505b892595d32d69
+
+
+# Get Libvirt version
+# getversion
+# getLibVersion
+
+# Get CPU information
+# getCPUMap
+# getMaxVcpus
+
+# Get free memory
+# getFreeMemory
+# getMemoryParameters
+
+#
