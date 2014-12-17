906264636c1733ee1d398670df100e458083e20d
sayalilunkad <sayali.92720@gmail.com>
2014-12-18 00:58:18 +0530

    Add doc strings

--- stacktrain/vm_tasks.py
+++ stacktrain/vm_tasks.py
@@ -5,25 +5,35 @@ import libvirt
 
 class Domain(object):
     def __init__(self):
-        uri = 'qemu:///system'
+        '''Connects to Libvirt
+	'''
+	uri = 'qemu:///system'
         self.conn = libvirt.open(uri)
 
     def power_on(self, domain_name):
-        dom0 = self.conn.lookupByName(domain_name)
-        dom0.create()
+	'''Powers on the required VM
+	'''
+        virDomain_obj = self.conn.lookupByName(domain_name)
+        virDomain_obj.create()
 
     def power_off(self, domain_name):
-        dom0 = self.conn.lookupByName(domain_name)
-        dom0.shutdown()
+        '''Powers off the required VM
+	'''
+	virDomain_obj = self.conn.lookupByName(domain_name)
+        virDomain_obj.shutdown()
 
     def create_domain(self, domain_file):
-        fhandle = open(domain_file)
+        '''Creates a VM as per XML description
+	'''
+	fhandle = open(domain_file)
         xml_description = fhandle.read()
         virDomain_obj = self.conn.defineXML(xml_description)
 
     def destroy_domain(self, domain_name):
+	'''Powers off a VM if on and destroys VM completely
+	'''
         active_domains = self.conn.listDomainsID()
-        dom0 = self.conn.lookupByName(domain_name)
-        if dom0.ID() in active_domains:
-            dom0.destroy()
-        dom0.undefine()
+        virDomain_obj = self.conn.lookupByName(domain_name)
+        if virDomain_obj.ID() in active_domains:
+            virDomain_obj.destroy()
+        virDomain_obj.undefine()
