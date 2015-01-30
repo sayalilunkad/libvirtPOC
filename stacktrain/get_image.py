1d539f83d8275f6e50464b0c4afdd4e338738093
sayalilunkad <sayali.92720@gmail.com>
2015-01-30 23:43:17 +0530

    Corrects dir path and checks if ISO present

--- stacktrain/get_image.py
+++ stacktrain/get_image.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
+# Checks and retrieves required ISO image for distro.
 
 import os
+import sys
 import urllib
 
 DISTRO = 'ubuntu'
-CURRENT_DIR = os.getcwd()
+ABS_DIR = os.path.abspath(sys.argv[0]).rsplit('/', 1)[0]
 if DISTRO == 'ubuntu':
-    urllib.urlretrieve("http://releases.ubuntu.com/14.04/ubuntu-14.04.1-server-amd64.iso", "%s/osbash/img/ubuntu-14.04.1-server-amd64.iso" % CURRENT_DIR)
+    if os.path.exists("%s/osbash/img/ubuntu-14.04.1-server-amd64.iso"
+                      % ABS_DIR) is True:
+        print 'ISO present.'
+    else:
+        print 'Downloading ISO.'
+        urllib.urlretrieve("http://releases.ubuntu.com/14.04/ubuntu-14.04.1- \
+        server-amd64.iso", "%s/osbash/img/ubuntu-14.04.1-server-amd64.iso"
+                           % ABS_DIR)
