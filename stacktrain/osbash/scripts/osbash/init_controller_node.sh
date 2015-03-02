cd31c5a8cfa495186e08a05f21f485d0af8986ee
sayalilunkad <sayali.92720@gmail.com>
2015-03-03 00:36:29 +0530

    Restores osbash scripts

--- stacktrain/osbash/scripts/osbash/init_controller_node.sh
+++ stacktrain/osbash/scripts/osbash/init_controller_node.sh
@@ -10,26 +10,22 @@ source "$CONFIG_DIR/credentials"
 source "$LIB_DIR/functions.guest"
 
 # Determine hostname from script name
-#re=".*init_(.*)_node.sh$"
-#if [[ $0 =~ $re ]]; then
-#    NODE_NAME=${BASH_REMATCH[1]}
-#    NODE_NAME="${NODE_NAME}"
-#else
-#    echo "ERROR Unable to determine hostname"
-#    exit 1
-#fi
-#
-#indicate_current_auto
-#
-#exec_logfile
-#
-## Set hostname for now and for rebooted system
-sudo hostname "controller" > /dev/null
-echo "controller" | sudo tee /etc/hostname > /dev/null
+re=".*init_(.*)_node.sh$"
+if [[ $0 =~ $re ]]; then
+    NODE_NAME=${BASH_REMATCH[1]}
+    NODE_NAME="${NODE_NAME}"
+else
+    echo "ERROR Unable to determine hostname"
+    exit 1
+fi
 
+indicate_current_auto
 
-#sudo hostname "$NODE_NAME" >/dev/null
-#echo "$NODE_NAME" | sudo tee /etc/hostname > /dev/null
-#
-#Configure network interfaces
+exec_logfile
+
+# Set hostname for now and for rebooted system
+sudo hostname "$NODE_NAME" >/dev/null
+echo "$NODE_NAME" | sudo tee /etc/hostname > /dev/null
+
+# Configure network interfaces
 config_network
